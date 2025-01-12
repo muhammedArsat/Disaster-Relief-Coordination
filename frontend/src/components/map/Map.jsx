14ed30feec29db346b49006318d4b5cb1b1ea511
muhammedArsat <ars21.code@gmail.com>
2025-01-12 21:52:38 +0530

    relief page

---
+++ frontend/src/components/map/Map.jsx
@@ -0,0 +1,26 @@
+// src/Map.js
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+  const position = [51.505, -0.09];
+
+  return (
+    <div style={{ position: 'relative', zIndex: '0' }}> {/* Ensure the parent has a positioning context */}
+      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%', zIndex: '0' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
