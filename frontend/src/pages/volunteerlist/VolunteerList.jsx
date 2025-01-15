71e6d3ad909a85eea7f206434d0b09a30402b7f4
Kavin778 <kavin.m6578@gmail.com>
2025-01-15 13:27:30 +0530

    first commit

--- frontend/src/pages/volunteerlist/VolunteerList.jsx
+++ frontend/src/pages/volunteerlist/VolunteerList.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
 
 const VolunteerList = () => {
   return (
-    <div>
-      
+    <div className="flex min-h-screen">
+      <Navbar/>
+      <main className="flex-1 mt-24 sm:mt-0 ml-0 sm:ml-64  p-4 bg-gray-600">
+      <h1 className="tex-2xl font-bold text-gray-400"></h1>
+      </main>
     </div>
   )
 }
 
-export default VolunteerList
+export default VolunteerList;
