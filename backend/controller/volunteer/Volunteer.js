501425d3f0fe102278e1d93ae8a62a5b303c7108
Muhammed Arsath <ars.code21@gmail.com>
2025-01-28 16:12:15 +0530

    add new Volunteers

--- backend/controller/volunteer/Volunteer.js
+++ backend/controller/volunteer/Volunteer.js
@@ -3,8 +3,13 @@ const VolunteerModal = require('../../modal/volunteer/VolunteerModal');
 //localhost:3000/api/v1/volunteer (POST)
 exports.addNewVolunteer = async (req, res, next) => {
     try {
+    const{name, age, role, skill, vehicle, phone, gender} = req.body;
+    const newVolunteer = new VolunteerModal({
+        name,age,vehicle,skill,role,phone,gender,createdAt:new Date()
+    })
 
-        
+    await newVolunteer.save()
+    res.status(201).json({message:"successfully new volunteer added", newVolunteer});
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
