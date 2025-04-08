a75d1db4dfa102dd247e478906e1a6f7eecef991
hamsa-09 <hamsavardhinibaskar@gmail.com>
2025-04-08 15:45:50 +0530

    getById

--- backend/controller/post/Post.js
+++ backend/controller/post/Post.js
@@ -11,8 +11,25 @@ exports.getAllPosts=async(req,res,next)=>{
         res.status(500).json({
         message:"Internal Server Error"
         })
-
     }
+}
+exports.getAllPostsById=async(req,res,next)=>{
+    try{
+        const postId=req.params.id;
+        const post=await postModal.findById(postId);
+        if(!post){
+            return res.status(404).json({message:"Post not found"});
+        }
+        res.status(200).json({
+            post
+        })
+    }
+        catch(error){
+            res.status(500).json({
+            message:"Internal Server Error"
+            })
+        }
+
 }
 
 exports.postForm = async(req,res,next)=>{
--- backend/routes/PostRoutes.js
+++ backend/routes/PostRoutes.js
@@ -4,5 +4,5 @@ const postController = require("../controller/post/Post");
 
 router.get("/post",postController.getAllPosts);
 router.post("/post",postController.postForm);
-
+router.get("/post/:id",postController.getAllPostsById)
 module.exports=router;
